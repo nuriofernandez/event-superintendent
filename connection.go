95002a42fa181aeb9df6d3932ee54a99df33c888
Nurio Fernández <personal@nurio.me>
2022-01-28 17:47:08 -0500

    Moved per-connection logic to a dedicated file

---
+++ connection.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func handleConection(conn net.Conn) {
+	// Notify new connections. 🔊
+	ip := conn.RemoteAddr().String()
+	fmt.Printf("<%s> Has been connected!\n", ip)
+
+	// Process data until the end of the process. 🔁
+	for {
+		message, err := bufio.NewReader(conn).ReadString('\n')
+
+		// Detect network errors and close connection. 👋
+		if err != nil {
+			fmt.Printf("<%s> Has been disconnected!\n", ip)
+			conn.Close()
+			return
+		}
+
+		// Log the received message. 🔊
+		logMsg := string(message[:len(message)-2]) // Remove \n from the string
+		fmt.Printf("<%s> : %s\n", ip, logMsg)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 )
@@ -13,26 +12,11 @@ func main() {
 	ln, _ := net.Listen("tcp", ":8000")
 	fmt.Printf("Listening connections at %s!\n", ln.Addr().String())
 
-	// Accept connection. 🤝
-	conn, _ := ln.Accept()
-
-	// Notify new connections. 🔊
-	ip := conn.RemoteAddr().String()
-	fmt.Printf("<%s> Has been connected!\n", ip)
-
-	// Process data until the end of the process. 🔁
 	for {
-		message, err := bufio.NewReader(conn).ReadString('\n')
+		// Accept connection. 🤝
+		conn, _ := ln.Accept()
 
-		// Detect network errors and close connection. 👋
-		if err != nil {
-			fmt.Printf("<%s> Has been disconnected!\n", ip)
-			conn.Close()
-			return
-		}
-
-		// Log the received message. 🔊
-		logMsg := string(message[:len(message)-2]) // Remove \n from the string
-		fmt.Printf("<%s> : %s\n", ip, logMsg)
+		// Handle the connection async. 🛤
+		go handleConection(conn)
 	}
 }
